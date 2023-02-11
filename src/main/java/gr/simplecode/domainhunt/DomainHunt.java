3dedb8daa66a13eae2b26d46ffcadf5fb29c1742
rept0id <rad@simplecode.gr>
2023-02-11 16:16:51 +0200

    init

---
+++ src/main/java/gr/simplecode/domainhunt/DomainHunt.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package gr.simplecode.domainhunt;
+
+/**
+ *
+ * @author rrr
+ */
+public class DomainHunt {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
